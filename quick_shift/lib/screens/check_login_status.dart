c5e3c99c28df4265282a57583b43ac641066b685
Srijon Mallick <srijonmallick.2000@gmail.com>
2022-11-12 11:49:42 +0530

    test dashboard code added

--- quick_shift/lib/screens/check_login_status.dart
+++ quick_shift/lib/screens/check_login_status.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:quick_shift/screens/auth_page.dart';
 import 'package:quick_shift/screens/home_page.dart';
+import 'package:quick_shift/screens/mobile_body.dart';
+import 'package:quick_shift/screens/desktop_body.dart';
 
 class CheckLoginStatus extends StatelessWidget {
   const CheckLoginStatus({super.key});
@@ -13,7 +15,9 @@ class CheckLoginStatus extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const HomePage();
+            //return const HomePage();
+            return const DesktopScaffold();
+            // return const MobileScaffold();
           } else {
             return const AuthPage();
           }
